7c1bfe2431467b41368ecd067676683b70d3ade9
Jaemar Joseph Ramos <jaemar.ramos@gmail.com>
2011-03-30 17:19:35 +0800

    initialized content

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskerV3::Application.config.secret_token = '4ca976eac38c2a0f47a3358d1335b088a2809043549f70d5ad33d7c4dee3a2bfa455bc0e550206183d0725c0101fa0749f101710d31a13d1f005d832006824af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TaskerV3::Application.config.session_store :cookie_store, :key => '_Tasker-v.3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TaskerV3::Application.config.session_store :active_record_store
